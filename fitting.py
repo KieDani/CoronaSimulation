8883f8c44d8eecdf7977ac8b9b79c98818964656
Daniel Kienzle <kienzledaniel@googlemail.com>
2020-04-11 12:49:18 +0200

    scaled the data

--- fitting.py
+++ fitting.py
@@ -1,5 +1,35 @@
 import numpy as np
 import main as ma
+import constants as const
 from numba import jit
+
+# Data from https://www.n-tv.de/infografik/Coronavirus-aktuelle-Zahlen-Daten-zur-Epidemie-in-Deutschland-Europa-und-der-Welt-article21604983.html
+# from 08.03.2020 to 10.04.2020
+number_sick_original = [1035, 1180, 1563, 1899, 2746, 3675, 4599, 5796, 7232, 9375, 12300, 15305, 19655, 22189, 21963, 26102, 30014, 34252, 37301, 42968, 48241, 52584, 48469, 50271, 53389, 57048, 60483, 63595, 65798, 66767, 67922, 59823, 60326, 61340]
+number_immune_original = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2809, 2809, 2809, 2809, 5600, 5600, 5600, 5600, 13500, 16100, 18700, 21400, 23800, 26400, 28700, 30600, 33300, 46300, 50000, 53913]
+population_germany = 80000000
+faktor_actual_cases = 10
+
+
+number_sick = np.asarray(number_sick_original) * faktor_actual_cases
+number_immune = np.asarray(number_immune_original) * 10
+
+number_sick = number_sick / float(population_germany) * const.numPop
+
+print(number_sick)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

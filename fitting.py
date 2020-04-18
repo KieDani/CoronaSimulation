1b3be5c02c92d2ad22797dad2395896204e9066e
Daniel Kienzle <kienzledaniel@googlemail.com>
2020-04-18 23:55:39 +0200

    tried to implement a version of the genetic algorithm that keeps the population sorted and only keeps the best individuals

--- fitting.py
+++ fitting.py
@@ -15,7 +15,7 @@ faktor_actual_cases = 100
 
 
 number_sick = np.asarray(number_sick_original) * faktor_actual_cases
-number_immune = np.asarray(number_immune_original) * 10
+number_immune = np.asarray(number_immune_original) * faktor_actual_cases
 
 number_sick = number_sick / float(population_germany) * const.numPop
 
@@ -418,6 +418,185 @@ def fitting_genetic2(actual_number_sick, populationsize = 50, number_generations
 
     return best_ind
 
+# keeps the upper half of the generation for the next generation
+def fitting_genetic3(actual_number_sick, populationsize=6, number_generations=7, time_lockdown=21):
+    def crossover(parent1, parent2):
+        child1 = []
+        child2 = []
+        for i in range(len(parent1)):
+            r = np.random.rand()
+            if (r > 0.5):
+                child1.append(parent1[i])
+                child2.append(parent2[i])
+            else:
+                child1.append(parent2[i])
+                child2.append(parent1[i])
+        return child1, child2
+
+    def selection(population):
+        mating_pool = []
+        for i in range(len(population)):
+            index1 = np.random.randint(low=0, high=len(population))
+            index2 = np.random.randint(low=0, high=len(population))
+            while (index1 == index2):
+                index2 = np.random.randint(low=0, high=len(population) - 1)
+            if (population[index1][6] < population[index2][6]):
+                mating_pool.append(population[index1])
+            else:
+                mating_pool.append(population[index2])
+        return mating_pool
+
+    def mutation(individual, U_max, U_min, t_max, t_min, V_max, V_min):
+        # how big is the mutation
+        step_percent = 0.1
+        gen_index = np.random.randint(low=0, high=len(individual) - 1)
+        print(gen_index)
+        tmp = [U_max, U_min, t_max, t_min, V_max, V_min, U_max, U_min, t_max, t_min, V_max, V_min]
+        mut = 2 * (np.random.rand() - 0.5) * (tmp[2 * gen_index] - tmp[2 * gen_index + 1]) * step_percent
+        # ensure, that the values are in the alllowed area
+        while (individual[gen_index] + mut < tmp[2 * gen_index + 1] or individual[gen_index] + mut > tmp[
+            2 * gen_index]):
+            mut = 2 * (np.random.rand() - 0.5) * (tmp[2 * gen_index] - tmp[2 * gen_index + 1]) * step_percent
+        individual[gen_index] += mut
+        return individual
+
+    def sort_population(population):
+        for i in range(1, len(population)):
+            to_be_sorted = population[i]
+            j = i
+            while (j > 0) and (population[j - 1][-1] > to_be_sorted[-1]):
+                population[j] = population[j - 1]
+                j -= 1
+            population[j] = to_be_sorted
+        return population
+
+
+
+    U_min = 0.5
+    U_max = 0.95
+    t_min = 0.0001
+    t_max = 0.25
+    V_min = 0.00001
+    V_max = 0.01
+    probability_mutation = 0.2
+    # initialize population
+    population = []
+    for i in range(populationsize):
+        # between 0.5 and 0.9530
+        U_before = np.random.rand() * (U_max - U_min) + U_min
+        U_after = np.random.rand() * (U_max - U_min) + U_min
+        # between 0.0001 and 0.15
+        t_before = np.random.rand() * (t_max - t_min) + t_min
+        t_after = np.random.rand() * (t_max - t_min) + t_min
+        # between 0.00001 and 0.01
+        V_before = np.random.rand() * (V_max - V_min) + V_min
+        V_after = np.random.rand() * (V_max - V_min) + V_min
+
+        U_array = list()
+        t_array = list()
+        V_array = list()
+        for j in range(len(actual_number_sick[:time_lockdown])):
+            U_array.append(U_before)
+            t_array.append(t_before)
+            V_array.append(V_before)
+        for j in range(len(actual_number_sick[time_lockdown:])):
+            U_array.append(U_after)
+            t_array.append(t_after)
+            V_array.append(V_after)
+        result = simulate_multi2(U_array, t_array, V_array, numberOfDays=len(actual_number_sick))
+        # print(result)
+        # print(actual_number_sick)
+        fitness = np.linalg.norm(actual_number_sick - result)
+
+        ind = [U_before, t_before, V_before, U_after, t_after, V_after, fitness]
+        population.append(ind)
+
+    # find best individual
+    # TODO check if I need deepcopy
+    best_ind = population[0].copy()
+    best_ind_fitness = best_ind[6]
+    # print(population)
+    # print(best_ind)
+    # print(best_ind_fitness)
+    for i in range(1, len(population)):
+        if (population[i][6] < best_ind_fitness):
+            # TODO check if I need deepcopy
+            best_ind = population[i].copy()
+            best_ind_fitness = best_ind[6]
+            print('Fitness: ' + str(best_ind_fitness))
+
+    # create new generations
+    for gen in range(1, number_generations + 1):
+        print('Generation: ' + str(gen))
+        mating_pool = selection(population)
+        for i in range(0, len(mating_pool), 2):
+            parent1 = mating_pool[i]
+            if (i + 1 < len(mating_pool)):
+                parent2 = mating_pool[i + 1]
+            else:
+                parent2 = mating_pool[0]
+            child1, child2 = crossover(parent1, parent2)
+            if (np.random.rand() < probability_mutation):
+                child1 = mutation(child1, U_max, U_min, t_max, t_min, V_max, V_min)
+                print('mutation 1')
+            if (np.random.rand() < probability_mutation):
+                child2 = mutation(child2, U_max, U_min, t_max, t_min, V_max, V_min)
+                print('mutation 2')
+
+            U_array1 = list()
+            t_array1 = list()
+            V_array1 = list()
+            U_array2 = list()
+            t_array2 = list()
+            V_array2 = list()
+            for j in range(len(actual_number_sick[:time_lockdown])):
+                U_array1.append(child1[0])
+                t_array1.append(child1[1])
+                V_array1.append(child1[2])
+                U_array2.append(child2[0])
+                t_array2.append(child2[1])
+                V_array2.append(child2[2])
+            for j in range(len(actual_number_sick[time_lockdown:])):
+                U_array1.append(child1[3])
+                t_array1.append(child1[4])
+                V_array1.append(child1[5])
+                U_array2.append(child2[3])
+                t_array2.append(child2[4])
+                V_array2.append(child2[5])
+            result1 = simulate_multi2(U_array1, t_array1, V_array1, numberOfDays=len(actual_number_sick))
+            result2 = simulate_multi2(U_array2, t_array2, V_array2, numberOfDays=len(actual_number_sick))
+            fitness1 = np.linalg.norm(actual_number_sick - result1)
+            fitness2 = np.linalg.norm(actual_number_sick - result2)
+            child1[6] = fitness1
+            child2[6] = fitness2
+
+
+            population.append(child1)
+            population.append(child2)
+
+        population = sort_population(population)
+        population = population[:populationsize]
+
+
+        # search, if there is a new best individual
+        for i in range(1, len(population)):
+            if (population[i][6] < best_ind_fitness):
+                # TODO check if I need deepcopy
+                best_ind = population[i].copy()
+                best_ind_fitness = best_ind[6]
+                print('found new best individual')
+                print('Fitness: ' + str(best_ind_fitness))
+
+    # save result:
+    params = ['U_before', 't_before', 'V_before', 'U_after', 't_after', 'V_after', 'Fitness']
+    with open('parameter_genetic.txt', 'w') as f:
+        for i in range(len(best_ind)):
+            f.write(params[i] + ': ' + str(best_ind[i]) + '\n')
+
+    return best_ind
+
+
+
 
 
 
@@ -453,7 +632,7 @@ def main():
     number_trials = 5
     result = list()
     for trial in range(number_trials):
-        best_ind = fitting_genetic2(actual_number_sick=number_sick)
+        best_ind = fitting_genetic3(actual_number_sick=number_sick)
         result.append(best_ind)
         with open('parameter_genetic_overall_factor' + str(faktor_actual_cases) +'.txt', 'a') as f:
             f.write('Trial: ' + str(trial) + '\n')
